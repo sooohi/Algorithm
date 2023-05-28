dff81fc4a1d95bb298bc50d573b0f714a5180a67
sooohi <68193613+sooohi@users.noreply.github.com>
2023-05-28 23:55:28 +0900

    백준 1927번 문제풀이

---
+++ Solution/src/Baekjoon/Solution2.java
@@ -0,0 +1,41 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Solution2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if(num == 0) {
+                if(q.isEmpty())
+                    System.out.println(0);
+                else
+                    System.out.println(q.poll());
+            }
+            else
+                q.add(num);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
